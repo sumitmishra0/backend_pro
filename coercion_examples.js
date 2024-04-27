3746a3effa79a641ec26a7bf64f51a17cec38bdd
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-04-27 18:47:02 +0530

    Create coercion_examples.js
    write some special cases in coercion how object is converted to string and giving output [Object object]

---
+++ coercion_examples.js
@@ -0,0 +1,9 @@
+// == does coercion
+// === doesn't coercion
+
+let obj = {x: 10, y : 20};
+let num = 10;
+
+console.log(`My num is ${num}`);
+console.log(`My obj is ${obj}`);
+console.log("My obj is",obj);
