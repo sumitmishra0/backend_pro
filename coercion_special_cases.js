e281b7ebf23e385f41b7a035d1e6d2ecc19f2570
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-04-27 18:13:20 +0530

    Create coercion_special_cases.js

---
+++ coercion_special_cases.js
@@ -0,0 +1,15 @@
+console.log("" + 0)
+console.log("" + -0)
+console.log("" + []);
+console.log("" + {});
+console.log("" + [1,2,3]);
+console.log("" + [null, undefined]);
+console.log("" + [1,2,null, 4]);
+console.log(0 - "010"); // "010" converted to decimal number    // used 0 zero  
+console.log(0 - "O10");  // Used O alphabet
+console.log(0 - 010) // 010 converted to octal number
+console.log(0 - "0xb"); // "0xb" converted to hexadecimal number
+console.log(0 - 0xb); // "0xb" converted to hexadecimal number
+console.log(0 - []);
+console.log(1 - []);
+console.log(4 - [1]);
